1141168fe1c3a88455e91f355b76a7d1962457c9
WajihaTahir <wajihatahir1994@gmail.com>
2024-08-15 19:39:55 +0200

    created UI for chat conversation component

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./RightSidebar.css";
+import assets from "../../assets/assets";
 
 const RightSidebar = () => {
-  return <div></div>;
+  return (
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Richard Sanford <img src={assets.green_dot} className="dot" alt="" />
+        </h3>
+        <p>Hi, I am using chat app</p>
+      </div>
+    </div>
+  );
 };
 
 export default RightSidebar;
