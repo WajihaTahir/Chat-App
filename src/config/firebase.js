38006dcc34e2a8eaa0cabf56f800f0e99137585b
WajihaTahir <wajihatahir1994@gmail.com>
2024-08-16 14:46:01 +0200

    created signup, login, and logout functionality

---
+++ src/config/firebase.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbqEgmwfkFpCXWsC6gzkg1x8NzWjZ49CY",
+  authDomain: "chat-app-53be4.firebaseapp.com",
+  projectId: "chat-app-53be4",
+  storageBucket: "chat-app-53be4.appspot.com",
+  messagingSenderId: "712001649188",
+  appId: "1:712001649188:web:bd819f304d9967639936b1",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+  try {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = response.user;
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name: "",
+      avatar: "",
+      bio: "Hey, I am using chat-app",
+      lastSeen: Date.now(),
+    });
+    await setDoc(doc(db, "chats", user.uid), {
+      chatData: [],
+    });
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+export { signup, login, logout, auth, db };
